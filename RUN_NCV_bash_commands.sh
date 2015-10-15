93b8ab9be55b46fa9d3ea5c824a7e5704fb0284a
barbara <barbara@ib.usp.br>
2015-10-15 21:58:09 +0400

    parallel GNU works

--- RUN_NCV_bash_commands.sh
+++ RUN_NCV_bash_commands.sh
@@ -33,21 +33,21 @@
 #If cluster does not use SGE
 #############################
 
-BP=3000
-SLIDE=1500
-LOGS=~/NCV_dir_package/scratch/logs/
-    for CHROM in {1..22}; do    
-    INPUT=~/NCV_dir_package/input_data/chr${CHROM}/AC_13pops.Map50_100.TRF.SDs.pantro2.tsv.gz
-     CHIMPfd=~/NCV_dir_package/input_data/outgroup_files/fds.hg19_pantro2.$CHROM.tsv.gz
-     POSITIONS=~/NCV_dir_package/bins/scan_bin3Mb_chr${CHROM}.pos
-     NBINS=$(wc -l ${POSITIONS} | cut -f 1 -d ' ') # the number of bins for the chromosome
-     for i in `seq 1 ${NBINS}`; do
-          TMPDIR=~/NCV_dir_package/scratch/temp_files/${CHROM}/bin${i}/
-            POS=$(sed -n ${i}p ${POSITIONS})
+#BP=3000
+#SLIDE=1500
+#LOGS=~/NCV_dir_package/scratch/logs/
+#    for CHROM in {1..22}; do    
+#    INPUT=~/NCV_dir_package/input_data/chr${CHROM}/AC_13pops.Map50_100.TRF.SDs.pantro2.tsv.gz
+#     CHIMPfd=~/NCV_dir_package/input_data/outgroup_files/fds.hg19_pantro2.$CHROM.tsv.gz
+#     POSITIONS=~/NCV_dir_package/bins/scan_bin3Mb_chr${CHROM}.pos
+#     NBINS=$(wc -l ${POSITIONS} | cut -f 1 -d ' ') # the number of bins for the chromosome
+#     for i in `seq 1 ${NBINS}`; do
+#          TMPDIR=~/NCV_dir_package/scratch/temp_files/${CHROM}/bin${i}/
+#            POS=$(sed -n ${i}p ${POSITIONS})
 
-                ~/NCV_dir_package/scripts/run_ncv_allpops_master_script.sh ${INPUT} ${POS} ${BP} ${SLIDE} ${TMPDIR} ${CHIMPfd} ${i}  &   #hopefully the ampersand will do the trick for parallelizing.
-done
-done
+#                ~/NCV_dir_package/scripts/run_ncv_allpops_master_script.sh ${INPUT} ${POS} ${BP} ${SLIDE} ${TMPDIR} ${CHIMPfd} ${i}  &   #hopefully the ampersand will do the trick for parallelizing.
+#done
+#done
 #Without parallelizinh this is taking more than 50 to complete only a few jobs.
 #The block above works, but the  commnads are not running in parallel
 
@@ -77,9 +77,36 @@ done
 
 }
 
-
 export -f run_NCV
 
+parallel --gnu --joblog ~/NCV_dir_package/scratch/logs/log run_NCV ::: `seq 1 22` 
 
-parallel --gnu --joblog ~/NCV_dir_package/scratch/logs/log run_NCV ::: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
+#the onbe above works, but still it is very slow. I need to parallelize each bin.
+
+
+
+#function test_func {
+#i=$1
+#TMPDIR=~/NCV_dir_package/scratch/temp_files/${CHROM}/bin${i}/
+#POS=$(sed -n ${i}p ${POSITIONS})
+
+# ~/NCV_dir_package/scripts/run_ncv_allpops_master_script.sh ${INPUT} ${POS} ${BP} ${SLIDE} ${TMPDIR} ${CHIMPfd} ${i}  &
+#}
+
+#export -f test_func
+
+#for CHROM in {1..22}; do
+#BP=3000
+#SLIDE=1500
+#LOGS=~/NCV_dir_package/scratch/logs/
+#INPUT=~/NCV_dir_package/input_data/chr${CHROM}/AC_13pops.Map50_100.TRF.SDs.pantro2.tsv.gz
+#CHIMPfd=~/NCV_dir_package/input_data/outgroup_files/fds.hg19_pantro2.${CHROM}.tsv.gz
+#POSITIONS=~/NCV_dir_package/bins/scan_bin3Mb_chr${CHROM}.pos
+#NBINS=$(wc -l ${POSITIONS} | cut -f 1 -d ' ') # the number of bins for the chromosome
+#parallel --gnu --joblog ~/NCV_dir_package/scratch/logs/log test_func ::: `seq 1 $NBINS`
+#done
+
+
+
+
 
