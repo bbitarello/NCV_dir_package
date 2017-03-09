5d71a2f47c314a6498aa343fe47b8cc6fba40b0e
BARBARA BITARELLO <barbarabitarello@gmail.com>
2017-03-09 16:40:07 +0100

    enrich plot final version

--- scripts/enrich_plot.R
+++ scripts/enrich_plot.R
@@ -10,7 +10,7 @@ library(wesanderson)
 
 dt<-data.table(
 logP=rep(NA,72), 
-Comparison=c(rep("G/(G+I)",8), rep("Ex/G",8), rep("Ex/all",8), rep("NSyn/Ex",8), rep("Reg/all",8),rep("Reg/(Reg+G)",8), rep("Reg/(Reg+I)",8), rep("Ex/(Ex+Reg)",8), rep("NSyn/(NSyn+Reg)",8)),
+Comparison=c(rep("Genic/(Genic+Intergenic)",8), rep("Exonic/Genic",8), rep("Exonic/all",8), rep("NSyn/Exonic",8), rep("Regulatory/all",8),rep("Regulatory/(Regulatory+Genic)",8), rep("Regulatory/(Regulatory+Intergenic)",8), rep("Exonic/(Exonic+Regulatory)",8), rep("NSyn/(NSyn+Regulatory)",8)),
 POP=rep(c("LWK", "LWK", "YRI","YRI","GBR","GBR", "TSI","TSI"),9), Set=rep(c("Significant","Outlier"),36)
 )
 #problem, we havep=0, so log(0)=-Inf. Need to do something about this. 
@@ -21,26 +21,15 @@ p[which(p==0)]<-0.001
 dt$logP<- abs(log(p))
 factor(dt$POP, levels=c("LWK","YRI","GBR","TSI"))-> dt$POP
 factor(dt$Set)-> dt$Set
-factor(dt$Comparison, levels=c("G/(G+I)","Ex/G", "Ex/all","NSyn/Ex", "Reg/all", "Reg/(Reg+G)","Reg/(Reg+I)","Ex/(Ex+Reg)","NSyn/(NSyn+Reg)"))-> dt$Comparison
-
+factor(dt$Comparison, levels=c("Exonic/all","Exonic/Genic", "NSyn/Exonic", "Regulatory/all", "Exonic/(Exonic+Regulatory)","NSyn/(NSyn+Regulatory)","Regulatory/(Regulatory+Genic)","Regulatory/(Regulatory+Intergenic)","Genic/(Genic+Intergenic)"))-> dt$Comparison
+dt %>% dplyr::filter(Comparison %in% c("Exonic/all","Exonic/Genic", "NSyn/Exonic", "Regulatory/all", "Exonic/(Exonic+Regulatory)","NSyn/(NSyn+Regulatory)"))-> dt2
 #pdf('figures/test_enrich_plot_v1.pdf')
 #png('figures/test_enrich_plot_v2.png')
 
-tiff(file="figures/enrich.tiff", height = 80.5, width = 160, units = 'mm', compression = "lzw", res = 500)
-#this one works for sure
-#ggplot(dt, aes(x=Comparison, y=logP, colour=POP, shape=Set)) + geom_point() + theme(axis.text.x=element_text(angle=45, hjust=1))
-#works!!!
-#ggplot(dt, aes(x=Comparison, y=logP, colour=POP, shape=Set)) + geom_point(size=3, alpha = 7/10) + scale_color_manual(values = wes_palette(n=4, name="Royal2")) + scale_shape_manual(values=c(17,19)) + theme(axis.text.x=element_text(angle=45, hjust=1))
-c(abs(log(0.025)),abs(log(0.975)))->h
-a<-expression(paste0('-log(', italic('p'), ')'))
-#v1
-#ggplot(dt, aes(x=Comparison, y=logP, colour=POP, shape=Set)) + geom_point(size=2.5, alpha = 5/10, position = position_jitter(w = 0.5)) + scale_color_manual(values = c('RoyalBlue',"slateblue","darkolivegreen4", "darkolivegreen3")) + scale_shape_manual(values=c(17,19)) + theme(axis.text.x=element_text(angle=45, hjust=1), axis.title.x=element_blank()) + geom_hline(yintercept=c(abs(log(0.025)),abs(log(0.975))), color="darkgray", linetype="dashed") + ylab(expression(-log(italic(p))))
-
-
+tiff(file="figures/enrich2.tiff", height = 90.5, width = 150, units = 'mm', compression = "lzw", res = 500)
+h<-log(0.05);a<-expression(paste0('-log(', italic('p'), ')'))
+#ggplot(dt2, aes(x=Comparison, y=logP, colour=POP, shape=Set)) + geom_point(size=2.5, alpha = 5/10, position = position_jitter(w = 0.5)) + scale_color_manual(values = c('RoyalBlue',"slateblue","darkolivegreen4", "darkolivegreen3")) + scale_shape_manual(values=c(17,19)) + theme(axis.text.x=element_text(angle=45, hjust=1), axis.title.x=element_blank()) + geom_hline(yintercept=abs(log(0.05)), color="darkgray", linetype="dashed") + ylab(expression(-log(italic(p))))
 #v2
-ggplot(dt, aes(x=Comparison, y=logP, colour=POP, shape=Set)) + geom_point(size=2.5, alpha = 5/10, position = position_jitter(w = 0.5)) + scale_color_manual(values = c('RoyalBlue',"slateblue","darkolivegreen4", "darkolivegreen3")) + scale_shape_manual(values=c(17,19)) + theme(axis.text.x=element_text(angle=45, hjust=1)) + geom_hline(yintercept=c(abs(log(0.025)),abs(log(0.975))), color="darkgray", linetype="dashed") +  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title.x=element_blank()) + ylab(expression(-log(italic(p))))
-
-
+ggplot(dt2, aes(x=Comparison, y=logP, colour=POP, shape=Set)) + geom_point(size=2.5, alpha = 5/10, position = position_jitter(w = 0.5)) + scale_color_manual(values = c('RoyalBlue',"slateblue","darkolivegreen4", "darkolivegreen3")) + scale_shape_manual(values=c(17,19)) + theme_bw() +theme(axis.text.x=element_text(angle=45, hjust=1), plot.background = element_blank(),panel.grid.major = element_blank(),panel.grid.minor = element_blank()) + theme(panel.border= element_blank()) + theme(axis.line.y = element_line(color="black", size = 0.5),axis.title.x=element_blank(), axis.line.x = element_line(color="black", size = 0.5)) + geom_hline(yintercept=abs(log(0.05)), color="darkgray", linetype="dashed") + ylab(expression(-log(italic(p)))) +  coord_cartesian(ylim=c(00,8))
 dev.off()
 
